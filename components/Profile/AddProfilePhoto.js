ad3c6c1ed115b3caa0e8ce625021c7ac2abbd4ed
charan <charan9487@gmail.com>
2021-01-11 15:54:34 +0530

    added profile info

---
+++ components/Profile/AddProfilePhoto.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  StyleSheet, Image, Dimensions
+} from 'react-native';
+
+const AddProfilePhoto = () => {
+  return (
+
+    <Image
+      // eslint-disable-next-line
+      source={require("../../assets/user1.jpg")}
+      style={styles.Profile_Info}
+    />
+
+  );
+};
+
+export default AddProfilePhoto;
+
+const styles = StyleSheet.create({
+  Profile_Info: {
+    width: Dimensions.get('window').width / 4,
+    height: Dimensions.get('window').width / 4,
+    borderWidth: 1,
+    borderRadius: Dimensions.get('window').width / 4,
+    marginLeft: '5%',
+    // width: Dimensions.get('window').width / 3,
+  },
+
+});
--- components/Profile/DisplayProfile.js
+++ components/Profile/DisplayProfile.js
@@ -1,25 +1,87 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectUserProfile } from '../../redux-sagas/user/user.selector';
 import MainContainer from '../../screens/MainContainer';
+// import IconComponent from '../Icon';
+import TextComponent from '../TextComponent';
+import AddProfilePhoto from './AddProfilePhoto';
 
 const DisplayProfile = ({ userProfile }) => {
-  console.log(userProfile.photo);
+  const hasProfilePhoto = !!userProfile.photo.includes('cdninstagram');
   return (
     <MainContainer>
-      <Image
-        source={{ uri: userProfile.photo }}
-        style={{ height: 100, width: 100, backgroundColor: 'red' }}
-      />
+      <View style={styles.Profile_Info_Container}>
+        <View style={styles.Profile_Photo}>
+          {hasProfilePhoto && <AddProfilePhoto />}
+        </View>
+        <View style={styles.Profile_Info_Details}>
+          <TouchableOpacity onPress={() => console.log('k')}>
+            <View>
+              <TextComponent
+                style={styles.Profile_Data}
+              >
+                0
+              </TextComponent>
+              <TextComponent style={{ fontFamily: 'Proxima-Regular' }}>
+                Posts
+              </TextComponent>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <View>
+              <TextComponent style={styles.Profile_Data}>
+                {userProfile && userProfile.followers.length}
+              </TextComponent>
+              <TextComponent style={{ fontFamily: 'Proxima-Regular' }}>Followers</TextComponent>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <View>
+              <TextComponent style={styles.Profile_Data}>
+                {userProfile && userProfile.following.length}
+              </TextComponent>
+              <TextComponent style={{ fontFamily: 'Proxima-Regular' }}>Following</TextComponent>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
     </MainContainer>
   );
 };
 
 const mapStateToProps = createStructuredSelector({
-  userProfile: selectUserProfile
-})
+  userProfile: selectUserProfile,
+});
 export default connect(mapStateToProps)(DisplayProfile);
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  Profile_Info_Container: {
+    width: '100%',
+    paddingVertical: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  Profile_Info_Details: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexBasis: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width / 4,
+    alignItems: 'center',
+    marginRight: '5%',
+  },
+  Profile_Photo: {
+    width: Dimensions.get('window').width / 3,
+  },
+  Profile_Data: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 19,
+  },
+
+});
